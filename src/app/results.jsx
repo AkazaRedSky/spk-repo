95ec8f8a29a9b159ea2e4245f8bffeaceb8c542a
AkazaRedSky <90970780+AkazaRedSky@users.noreply.github.com>
2023-11-27 14:46:28 +0800

    feat: fixed result

--- src/app/results.jsx
+++ src/app/results.jsx
@@ -8,6 +8,14 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Checkbox,
 } from "@nextui-org/react";
 import { SWARAContext } from "./context/swaracontext";
 
@@ -151,6 +159,17 @@ export default function Results() {
       label: "Auxiliary Components",
     },
   ];
+  const [username, setUsername] = useState();
+
+  const handleusername = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    setUsername();
+    setResultVisibility(false);
+  };
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <div>
@@ -162,36 +181,82 @@ export default function Results() {
       </div>
       {resultVisibility ? (
         <>
-        
           <Table aria-label="tabelcalcfinal">
             <TableHeader columns={columns}>
-              {(column) => (
+              <TableColumn className="text-center">Select</TableColumn>
+              {columns.map((column) => (
                 <TableColumn className="text-center" key={column.key}>
                   {column.label}
                 </TableColumn>
-              )}
+              ))}
             </TableHeader>
-            <TableBody items={maxBudget ? VGALists.filter(item => item.price < maxBudget) : VGALists}>
+            <TableBody
+              items={
+                maxBudget
+                  ? VGALists.filter((item) => item.price < maxBudget)
+                  : VGALists
+              }
+            >
               {(item) => (
                 <TableRow key={item.key}>
-                  {(columnKey) => (
+                  <TableCell className="text-center">
+                    <Checkbox size="md"></Checkbox>
+                  </TableCell>
+                  {columns.map((column) => (
                     <TableCell
                       className="text-center"
-                      key={`${item.key}-${columnKey}`}
+                      key={`${item.key}-${column.key}`}
                     >
-                      {item[columnKey]}
+                      {item[column.key]}
                     </TableCell>
-                  )}
+                  ))}
                 </TableRow>
               )}
             </TableBody>
           </Table>
 
           <button onClick={(e) => setResultVisibility(false)}> - </button>
+
+          <div className="relative py-3">
+            <p className="text-left font-bold text-base text-slate-800">Name</p>
+            <div className="gap-y-2">
+              <input
+                type="text"
+                className="border-slate-700 border-2 bg-white font-semibold text-center select-none text-sm text-black"
+                value={username}
+                onChange={handleusername}
+              />
+              <div className="flex gap-y-2">
+                <Button
+                  color="success"
+                  className="flex text-center text-sm text-white"
+                  onClick={handleSubmit}
+                  onPress={onOpen}
+                >
+                  Submit
+                </Button>
+              </div>
+            </div>
+          </div>
         </>
       ) : (
         <> </>
       )}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Success</ModalHeader>
+              <ModalBody>Submitted Succesfully</ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
